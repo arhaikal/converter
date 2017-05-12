01292e8930be66430f32092bb0a1786f6a153955
Andre Haikal <andrehaykal@gmail.com>
2017-05-12 10:23:54 +0300

    validations

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,12 +2,11 @@ class Exchange < ApplicationRecord
   belongs_to :user
   validates_presence_of :target_currency,
                         :base_currency
-  validates :amount, {minimum: 1, maximum: 100000000},
-                     presence: true,
-                     numericality: true
-  validates :waiting_time, {minimum: 1, maximum: 250},
-                           presence: true,
-                           numericality: { only_integer: true }
+  validates :amount, inclusion: { in: 1..100000000,
+                                  message: "The amount can be between 1 and 100000000" },
+
+  validates :waiting_time, inclusion: { in: 1..250,
+                                        message: "The amount must be between 1 and 250" }
 
 
 end
