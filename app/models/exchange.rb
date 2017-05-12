73b7b76acc00a83c4074eeca136ae9021f2c3314
Andre Haikal <andrehaykal@gmail.com>
2017-05-12 10:26:55 +0300

    validations

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -3,7 +3,7 @@ class Exchange < ApplicationRecord
   validates_presence_of :target_currency,
                         :base_currency
   validates :amount, inclusion: { in: 1..100000000,
-                                  message: "The amount can be between 1 and 100000000" },
+                                  message: "The amount can be between 1 and 100000000" }
 
   validates :waiting_time, inclusion: { in: 1..250,
                                         message: "The amount must be between 1 and 250" }
